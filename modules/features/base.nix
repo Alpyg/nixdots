d73b548e7c0ae864b5c00a5cdd523102d225e628
Alpyg <alpyg@pm.me>
2026-07-14 14:48:29 -0400

    move mangohud config

--- modules/features/base.nix
+++ modules/features/base.nix
@@ -47,59 +47,6 @@
       home.shellAliases = {
         ls = "eza";
       };
-      xdg.configFile."MangoHud/MangoHud.conf".text = ''
-        legacy_layout=false
-
-        horizontal
-        round_corners=10
-        background_alpha=0.0
-        background_color=1E1E2E
-        table_columns=3
-
-        font_size=16
-        text_color=CDD6F4
-        position=top-left
-
-        toggle_hud=Shift_R+F1
-        hud_compact
-
-        gpu_stats
-        gpu_text=GPU
-        gpu_load_change
-        gpu_load_value=50,90
-        gpu_load_color=FFFFFF,FFAA7F,CC0000
-        gpu_temp
-        gpu_color=A6E3A1
-
-        cpu_stats
-        cpu_text=CPU
-        cpu_load_change
-        cpu_load_value=50,90
-        cpu_load_color=FFFFFF,FFAA7F,CC0000
-        cpu_temp
-        cpu_color=89B4F4
-
-        vram
-        vram_color=AD64C1
-
-        ram
-        ram_color=F5C2E7
-
-        fps
-        frame_timing
-        frametime_color=A6E3A1
-        fps_limit_method=early
-        toggle_fps_limit=none
-
-        fps_limit=0
-        fps_color_change
-        fps_color=B22222,FDFD09,39F900
-        fps_value=30,60
-        #offset=0
-
-        output_folder="/home/alpyg"
-        toggle_logging=none
-      '';
 
       programs.bat = {
         enable = true;
